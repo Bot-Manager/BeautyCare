e8e8a54f5953c0324f61b6861b91cba14c8f3edc
AlexRpg-86 <54549313+AlexRpg-86@users.noreply.github.com>
2019-10-31 17:56:13 -0300

    Base proyecto

---
+++ servicios/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Servicios
+
+
+def lista_servicio(request):
+    lista_servicio = Servicios.objects.all()
+
+    context = {'lista_servicio' : lista_servicio ,}
+
+    return render(request , 'Servicios/lista.html' , context)
+
+
+def detalle_servicio(request , slug):
+    pass
